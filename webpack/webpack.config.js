eef0eb14638de933d7c9c71eab8b36b6182eeebd
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2020-10-07 09:02:15 -0500

    clean dist directory

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
+const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const { paths, common } = require('./webpack.common');
 
@@ -37,6 +39,8 @@ const prodConfig = merge([
       ],
     },
     plugins: [
+      new webpack.ProgressPlugin(),
+      new CleanWebpackPlugin(),
       new MiniCssExtractPlugin({
         filename: 'neo4jd3.min.css',
       }),
